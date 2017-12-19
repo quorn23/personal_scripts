da6b6c968aa58f01127c351a4426478e2feef0ae
Sebastian S <sualfred@users.noreply.github.com>
2017-12-19 11:09:57 +0100

    update

--- convert-letsencrypt-to-pfx-and-scp/scp-ssl.sh
+++ convert-letsencrypt-to-pfx-and-scp/scp-ssl.sh
@@ -4,3 +4,7 @@
 #Replace <domain name> and <path>
 cd /etc/letsencrypt/live/<domain name>/ && sudo openssl pkcs12 -export -out cert.pfx -inkey privkey.pem -in cert.pem -certfile chain.pem -passout pass:Emby
 scp /etc/letsencrypt/live/<domain name>/cert.pfx root@192.168.0.0:/var/www/<path>
+#To skip the password use sshpass (apt-get install sshpass)
+#Example:
+#sshpass -p "MYPASSWORD" scp /etc/letsencrypt/live/<domain name>/cert.pfx root@192.168.0.0:/var/www/<path>
+
