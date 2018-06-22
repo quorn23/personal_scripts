9f8a88b3b193fa500f172638a8b040a081db2dfc
Sebastian S <su4lfred@gmail.com>
2018-06-22 09:48:19 +0200

    sendwol.sh

--- sendwol/sendwol.sh
+++ sendwol/sendwol.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-check_ip=('192.168.2.248')
+check_mac=('00:C2:C6:CA:2F:79' 'B8:AE:ED:EB:07:03')
 wol_mac=('70:85:c2:02:c6:31')
 loop_sleep_timer=5s
 
@@ -11,42 +11,27 @@ do
         unset ping_client
         unset ping_target
 
-        hyperion_status=$(service hyperion status | grep running)
-
         # Check if client IPs are online
-        for client in "${check_ip[@]}"
+        for client in "${check_mac[@]}"
         do
-                ping_client=$(ping "$client" -c 1 | grep "1 received")
+                #ping_client=$(ping "$client" -c 1 | grep "1 received")
+                ping_client=$(arp | grep "$client")
+
                 if ! [ -z "$ping_client" ]; then
-
-                        found_client="$client"
-
-                        # Start Hyperion
-                        if [ -z "$hyperion_status" ]; then
-                                echo "Client $client found and Hyperion is not running. Starting Hyperion."
-                                sudo service hyperion start
-                        fi
-
+                        found_client=$(arp | grep "$client" | awk ' { print $1 } ')
                         break
-
                 fi
         done
 
-
-        # Stop Hyperion if no client is reachable
-        if [ -z "$found_client" ] && ! [ -z "$hyperion_status" ]; then
-                echo "Clients are offline and Hyperion is still running. Stopping Hyperion."
-                sudo service hyperion stop
-        fi
-
-
         # Wake all required devices as soon as one client IP has been found
         if ! [ -z "$found_client" ]; then
+
                 for wake_target in "${wol_mac[@]}"
                 do
                         # Ping target to check if it's already online
-                        wake_ip=$(arp | grep "$wake_target" | awk ' { print $1 } ')
-                        ping_target=$(ping "$wake_ip" -c 1 | grep "1 received")
+                        #wake_ip=$(arp | grep "$wake_target" | awk ' { print $1 } ')
+                        #ping_target=$(ping "$wake_ip" -c 1 | grep "1 received")
+                        ping_target=$(arp | grep "$wake_target")
 
                         # Send WOL if required
                         if [ -z "$ping_target" ]; then
@@ -55,6 +40,7 @@ do
                                 sleep 10s
                         fi
                 done
+
         fi
 
         # Wait for the next round
